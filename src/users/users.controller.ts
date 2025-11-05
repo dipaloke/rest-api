5e3d8fe5a1ecbca74ff3e33adac8e8527c20fd45
Dipaloke <dipalokebiswas96@gmail.com>
2025-11-05 18:27:07 +0600

    Chapter 2:(Basic working scheleton to handle user req): learned about basic controller functionalities, Created controllers to handle routes for GET, POST, PATCH & DELETE req using decoretors, learned to use @Body decoretor to access req body.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,47 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
-@Controller('users')
-export class UsersController {}
+@Controller('users') //* will handle /users route (parent route)
+export class UsersController {
+  @Get() //GET /users or /users?role?=value
+  findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    return [];
+  }
+  //   @Get('interns') //* GET /users/interns (will work)
+  //   findAllInterns() {
+  //     return [];
+  //   }
+
+  @Get(':id') //GET /users/:id
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  //   @Get('interns') //! GET /users/interns (will not work)
+  //   findAllInterns() {
+  //     return [];
+  //   }
+
+  @Post() //POST /users
+  create(@Body() user: object) {
+    return user;
+  }
+
+  @Patch(':id') //PATCH /users/:id
+  update(@Param('id') id: string, @Body() userUpdate: object) {
+    return { id, ...userUpdate };
+  }
+
+  @Delete(':id') //DELETE /users/:id
+  delete(@Param('id') id: string) {
+    return { id };
+  }
+}
