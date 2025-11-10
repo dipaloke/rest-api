2dcabffc2d0e6bd827ee66f2291fc23635fdb327
Dipaloke <dipalokebiswas96@gmail.com>
2025-11-10 11:36:10 +0600

    Feat Chapter 5: REST API: intelled prisma, prisma client, Connected with Mongo DB, Created DB module and services into prohject, Learned how to create resources all at once for Employees DB, deleted the dtos as type safty will come from prisma client, Created functionalities for CRUD operations and tested vis postman.

---
+++ src/employees/employees.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { DatabaseService } from 'src/database/database.service';
+
+@Injectable()
+export class EmployeesService {
+  constructor(private readonly databaseService: DatabaseService) {}
+
+  async create(createEmployeeDto: Prisma.EmployeesCreateInput) {
+    return this.databaseService.employees.create({
+      data: createEmployeeDto,
+    });
+  }
+
+  async findAll(role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    if (role)
+      return this.databaseService.employees.findMany({
+        where: {
+          role,
+        },
+      });
+    return this.databaseService.employees.findMany();
+  }
+
+  async findOne(id: string) {
+    return this.databaseService.employees.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async update(id: string, updateEmployeeDto: Prisma.EmployeesUpdateInput) {
+    return this.databaseService.employees.update({
+      where: {
+        id,
+      },
+      data: updateEmployeeDto,
+    });
+  }
+
+  async remove(id: string) {
+    return this.databaseService.employees.delete({
+      where: {
+        id,
+      },
+    });
+  }
+}
